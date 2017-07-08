99db11711cc3f46c7645b518fb87c98a6c423128
Katie Harron <katie@pibby.com>
2017-07-08 11:21:38 -0500

    Initial commit for v2, CSS missing

--- src/components/yields/V2/index.js
+++ src/components/yields/V2/index.js
@@ -1,7 +1,28 @@
+import style from './style.scss'
+import statusImg from 'images/status_image.png'
+
 const MainComponent = ()=>{
   return (
-    <div>
-      There
+    <div className={style.flex-col}>
+      <aside className={style.sidebar}>
+        <div className={style.sidebar__status}>
+          <div className={style.sidebar__status-image}>
+            <img src={statusImg} alt="Current Status" />
+          </div>
+          <p>This week I am:</p>
+          <form>
+            <select>
+              <option>Not working</option>
+              <option>Really Light</option>
+              <option>Kinda Light</option>
+              <option>Not that Busy</option>
+              <option>Kinda Busy</option>
+              <option>Really Busy</option>
+              <option>Slammed</option>
+            </select>
+          </form>
+        </div>
+      </aside>
     </div>
   )
 }
