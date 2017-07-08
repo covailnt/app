8b21f5d965450142200eb8622d494cf5f8666179
Katie Harron <katie@pibby.com>
2017-07-08 16:04:08 -0500

    Sidebar, begin header

--- src/components/yields/V2/index.js
+++ src/components/yields/V2/index.js
@@ -1,29 +1,67 @@
+import { Link } from 'react-router-dom'
 import style from './style.scss'
 import statusImg from 'images/status_image.png'
 
 const MainComponent = ()=>{
   return (
-    <div className={style.flex-col}>
-      <aside className={style.sidebar}>
-        <div className={style.sidebar__status}>
-          <div className={style.sidebar__status-image}>
-            <img src={statusImg} alt="Current Status" />
-          </div>
-          <p>This week I am:</p>
-          <form>
-            <select>
-              <option>Not working</option>
-              <option>Really Light</option>
-              <option>Kinda Light</option>
-              <option>Not that Busy</option>
-              <option>Kinda Busy</option>
-              <option>Really Busy</option>
-              <option>Slammed</option>
-            </select>
-          </form>
-        </div>
-      </aside>
-    </div>
+		<div>
+			<aside className={style.sidebar}>
+				<div className={style.sidebarStatus}>
+					<div className={style.sidebarStatusImage}>
+						<img src={statusImg} alt="Current Status" />
+					</div>
+				</div>
+				<div className={style.statusCnt}>
+					<p>This week I am:</p>
+					<form className={style.statusSelect}>
+						<select>
+							<option>Not working</option>
+							<option>Really Light</option>
+							<option>Kinda Light</option>
+							<option>Not that Busy</option>
+							<option>Kinda Busy</option>
+							<option>Really Busy</option>
+							<option>Slammed</option>
+						</select>
+						<button type="submit" className={style.btnPrimary}>Update Availability</button>
+					</form>
+				</div>
+
+				<nav className={style.nav}>
+					<ul className={style.sidebarNav}>
+						<li>
+							<Link to="profile">Profile</Link>
+						</li>
+						<li>
+							<Link to="contacts">Contacts <span className={style.badge}>1</span></Link>
+						</li>
+						<li>
+							<Link to="chat">Chat</Link>
+						</li>
+						<li>
+							<Link to="colab">Colab Sessions</Link>
+						</li>
+					</ul>
+				</nav>
+			</aside>
+
+			<header className={style.header}>
+				<div className={style.headerEdit}>
+					<p>
+						<Link to="exit">Exit edit mode</Link>
+						Profile: 20% complete
+					</p>
+				</div>
+
+				<div className={style.headerStats}>
+					Rank Earned 005
+					Rank Potential 99
+					<div className={style.headerLogout}>
+						<Link to="logout"><img src="//placehold.it/50x50" /></Link>
+					</div>
+				</div>
+			</header>
+		</div>
   )
 }
 
