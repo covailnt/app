db5c4094c53530ea15b63312eb98fa60b7aed519
Katie Harron <katie@pibby.com>
2017-07-09 10:00:45 -0500

    Profile begin

--- src/components/yields/V2/index.js
+++ src/components/yields/V2/index.js
@@ -9,6 +9,23 @@ const MainComponent = ()=>{
 		<div>
 			<SidebarComponent />
 			<HeaderComponent />
+
+			<section className={style.profile}>
+				<div className={style.profileCover}>
+					<p>Drop cover photo here</p>
+				</div>
+				<div className={style.profileMain}>
+					<h1>Username</h1>
+					<p>Specialty in <input type="text" placeholder="Enter city or specify Nomad" className={style.formField} /></p>
+				</div>
+				<div className={style.profilePhoto}>
+					<img src="//placehold.it/300x300" alt="Profile Photo" />
+				</div>
+				<div className={style.profileStats}>
+					005 Rank Earned
+					99/100 Potential Rank
+				</div>
+			</section>
 		</div>
   )
 }
