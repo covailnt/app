3bf0a1532be10d94a091af97bcd8d26b9ef341a7
protoEvangelion <ryantgarant@gmail.com>
2017-11-21 22:38:19 -0800

    Converted DropImage to styled component

--- src/components/groups/DropImage/DropImage.js
+++ src/components/groups/DropImage/DropImage.js
@@ -1,10 +1,31 @@
-import { Flex, Icon } from 'components/elements'
+import { Box, Flex, Icon } from 'components/elements'
 import PropTypes from 'prop-types'
 import React from 'react'
 import Dropzone from 'react-dropzone'
+import styled from 'styled-components'
 
-import classes from './DropImage.scss'
 import firebase from '.config'
+
+const StyledDropzone = styled(Dropzone)`
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  cursor: pointer;
+  max-width: 100%;
+  opacity: 1;
+  padding-top: 16px;
+  transition: opacity 1s;
+  font-size: 18px;
+  font-weight: bold;
+
+  &:hover {
+    opacity: 0.7;
+    transition: opacity 1s;
+  }
+`
 
 const DropImage = ({
   defaultImage,
@@ -47,18 +68,23 @@ const DropImage = ({
   }
 
   return (
-    <Flex align="center" direction="column" justify="center">
-      <Dropzone
-        className={classes.dropzone}
+    <Flex
+      align="center"
+      direction="column"
+      justify="center"
+      style={{ height: '100%' }}
+    >
+      <StyledDropzone
+        color="black"
         multiple={false}
         onDrop={onDrop.bind(this)}
         style={styles}
       >
         <Flex align="center" direction="column">
-          <div>{label}</div>
           <Icon fill="black" name="photo" />
+          <Box mt={3}>{label}</Box>
         </Flex>
-      </Dropzone>
+      </StyledDropzone>
     </Flex>
   )
 }
