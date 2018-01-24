6c8971749583c3e6c7b5da3300b0ced60d845f8b
protoEvangelion <ryantgarant@gmail.com>
2018-01-23 17:46:02 -0800

    Updated Home.js

--- src/app/pages/home/Home.js
+++ src/app/pages/home/Home.js
@@ -4,6 +4,15 @@ import { connectRedux } from '~/store'
 import { getUserData } from '~/utils'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import { Button as Btn } from 'rebass'
+
+const Button = Btn.extend`
+  transition: background 1s;
+  cursor: pointer;
+  &:hover {
+    transition: background-color 1s;
+  }
+`
 
 class Index extends Component {
   static async getInitialProps({ req, store }) {
@@ -21,6 +30,7 @@ class Index extends Component {
     const { user } = this.props
     return (
       <div>
+        <Button>Hello</Button>
         {user ? <UserMenu photo={user.picture} /> : <SignIn />}
         <Heading level={1}>You are Home</Heading>
       </div>
